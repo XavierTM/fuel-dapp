a1e99434deded94f0c3d983976eef13fcef77c6c
Xavier Mukodi <xaviermukodi@gmail.com>
2022-11-25 11:00:41 +0200

    added console.log to migrations/Token.js

--- api/contracts/migrations/Token.js
+++ api/contracts/migrations/Token.js
@@ -1,7 +1,10 @@
 // Help Truffle find `Token.sol` in the `/contracts` directory
 const Token = artifacts.require("Token");
 
-module.exports = function(deployer) {
+module.exports = async function(deployer) {
   // Command Truffle to deploy the Smart Contract
-  deployer.deploy(Token, "XAVITOKEN", "XVT", 0, 2000000);
+  const res = await deployer.deploy(Token, "XAVITOKEN", "XVT", 0, 2000000);
+
+  console.log(res);
+  
 };
